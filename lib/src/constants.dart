a401a07433f0a61434bca99d2fcbeb7367f9a57b
Bharat Sharma <bharat.sharma1809@gmail.com>
2021-02-08 01:38:52 +0530

    added clear and frosted glass constructors

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,9 +1,10 @@
 /// The default values used by the [GlassContainer]
 
-import 'package:flutter/widgets.dart' show Color, BorderRadius, BlendMode;
+import 'package:flutter/widgets.dart'
+    show Color, BorderRadius, BlendMode, LinearGradient, Alignment;
 
 /// The default value of the Gaussian Blur filter
-const double kBlur = 10.0;
+const double kBlur = 12.0;
 
 /// The constant value of the [borderRadius] property
 const BorderRadius kBorderRadius = BorderRadius.zero;
@@ -27,7 +28,27 @@ const Color kShadowColor = Color(0x33000000);
 const String kNoiseImage = 'assets/noise.png';
 
 /// Blend Color for frosted layer
-const Color kFrostBlendColor = Color(0xffffffff);
+const Color kFrostBlendColor = Color(0xFFFFFFFF);
 
 /// Blend mode for frosted layer
 const BlendMode kFrostBlendMode = BlendMode.difference;
+
+/// Default gradient fill of the containers
+const LinearGradient kGradientFill = LinearGradient(
+  colors: [Color(0x66FFFFFF), Color(0x1AFFFFFF)],
+  begin: Alignment.topLeft,
+  end: Alignment(0.80, 0.80),
+);
+
+/// Default border gradient fill of the containers
+const LinearGradient kBorderGradientFill = LinearGradient(
+  colors: [
+    Color(0x99FFFFFF),
+    Color(0x1AFFFFFF),
+    Color(0x1AF0FFFF),
+    Color(0x99F0FFFF)
+  ],
+  begin: Alignment(0.2, 0.0),
+  end: Alignment(1.0, 1.0),
+  stops: [0.0, 0.39, 0.40, 1.0],
+);
